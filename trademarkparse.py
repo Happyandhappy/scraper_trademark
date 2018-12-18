0d5f726e57bc9541f4430cc846ac65dda9f6c038
happyandhappy <greyfrapps@gmail.com>
2018-12-18 12:50:10 +0100

    update with csv

--- trademarkparse.py
+++ trademarkparse.py
@@ -7,74 +7,88 @@ BASE_URL = "https://search.ipaustralia.gov.au/trademarks/search/view/"
 
 start = time.time()
 isHeader = True
+session = requests.session()
+
+fieldnames = [    'ID',
+                  'Words',
+                  'IR number',
+                  'IR notification',
+                  'Kind',
+                  'Class',
+                  'Filing Date',
+                  'First report Date',
+                  'Registered From Date',
+                  'Registration Advertised Date',
+                  'Acception Advertised Date',
+                  'Acception Date',
+                  'Image',
+                  'Image description',
+                  'Priority Date',
+                  'Renewal Due Date',
+                  'Status',
+                  'Owner',
+                  'Address for service',
+                  'IR Contact',
+                  'History',
+                  'Goods and services',
+                  'Indexing constituents image',
+                  'Indexing constituents word',
+                  'Convention date',
+                  'Convention number',
+                  'Convention country'
+              ]
 
 def elapsed():
     return time.time() - start
 
-def ouputCSV(dict, filename):
-    global isHeader
-    fieldnames = [  'ID',
-                    'Words',
-                    'IR number',
-                    'IR notification',
-                    'Kind',
-                    'Class',
-                    'Filing Date',
-                    'First report Date',
-                    'Registered From Date',
-                    'Registration Advertised Date',
-                    'Acception Advertised Date',
-                    'Acception Date',
-                    'Image',
-                    'Image description',
-                    'Priority Date',
-                    'Renewal Due Date',
-                    'Status',
-                    'Owner',
-                    'Address for service',
-                    'IR contact',
-                    'History',
-                    'Goods and services',
-                    'Indexing constituents image',
-                    'Indexing constituents word',
-                    'Convention date',
-                    'Convention number',
-                    'Convention country'
-                ]
-    rowdict = {}
-    rowdict['ID'] = dict['ID'] if dict['ID'] else ''
-    rowdict['Words'] = dict['Words'] if dict['Words'] else ''
-    rowdict['IR number'] = dict['IR number'] if 'IR number' in dict else ''
-    rowdict['IR notification'] = dict['IR notification'] if 'IR notification' in dict else ''
-    rowdict['Kind'] = dict['Kind'] if 'Kind' in dict else ''
-    rowdict['Class'] = dict['Class'] if 'Class' in dict else ''
-    rowdict['Filing Date'] = dict['Filing'] if 'Filing' in dict else ''
-    rowdict['First report Date'] = dict['First report Date'] if 'First report Date' in dict else ''
-    rowdict['Registered From Date'] = dict['Registered from'] if 'Registered from' in dict else ''
-    rowdict['Registration Advertised Date'] = dict['Registration advertised'] if 'Registration advertised' in dict else ''
-    rowdict['Acception Advertised Date'] = dict['Acceptance advertised'] if 'Acceptance advertised' in dict else ''
-    rowdict['Acception Date'] = dict['Acceptance'] if 'Acceptance' in dict else ''
-    rowdict['Image'] = dict['Image'] if 'Image' in dict else ''
-    rowdict['Image description'] = dict['Image description'] if 'Image description' in dict else ''
-    rowdict['Priority Date'] = dict['Priority date'] if 'Priority date' in dict else ''
-    rowdict['Renewal Due Date'] = dict['Renewal due'] if 'Renewal due' in dict else ''
-    rowdict['Status'] = dict['Status'] if 'Status' in dict else ''
-    rowdict['Owner'] = dict['Owner'] if 'Owner' in dict else ''
-    rowdict['Address for service'] = dict['Address for service'] if 'Address for service' in dict else ''
-    rowdict['IR contact'] = dict['IR contact'] if 'IR contact' in dict else ''
-    rowdict['Goods and services'] = dict['Goods and services'] if 'Goods and services' in dict else ''
-    rowdict['Indexing constituents image'] = dict['Indexing constituents image'] if 'Indexing constituents image' in dict else ''
-    rowdict['Indexing constituents word'] = dict['Indexing constituents word'] if 'Indexing constituents word' in dict else ''
-    rowdict['Convention date'] = dict['Convention details']['Date'] if 'Convention details' in dict else ''
-    rowdict['Convention number'] = dict['Convention details']['Number'] if 'Convention details' in dict else ''
-    rowdict['Convention country'] = dict['Convention details']['Country'] if 'Convention details' in dict else ''
+def returnValue(key, dict):
+    return dict[key] if key in dict else ''
 
-    with open(filename, "a", newline='') as file:
-        csvWriter = csv.DictWriter(file, fieldnames=fieldnames, quoting=csv.QUOTE_NONNUMERIC)
+def AdjustData(dict):
+    rowdict = {}
+    rowdict['ID'] =  returnValue('ID', dict)
+    rowdict['Words'] = returnValue('Words', dict)
+    rowdict['IR number'] = returnValue('IR number', dict)
+    rowdict['IR notification'] = returnValue('IR notification', dict)
+    rowdict['Kind'] = returnValue('Kind', dict)
+    rowdict['Class'] = dict['Class'] if 'Class' in dict else dict['Classes']
+    rowdict['Filing Date'] = returnValue('Filing', dict)
+    rowdict['First report Date'] = returnValue('First report', dict)
+    rowdict['Registered From Date'] = returnValue('Registered from', dict)
+    rowdict['Registration Advertised Date'] = returnValue('Registration advertised', dict)
+    rowdict['Acception Advertised Date'] = returnValue('Acceptance advertised', dict)
+    rowdict['Acception Date'] = returnValue('Acceptance', dict)
+    rowdict['Image'] = returnValue('Image', dict)
+    rowdict['Image description'] = returnValue('Image description', dict)
+    rowdict['Priority Date'] = returnValue('Priority date', dict)
+    rowdict['Renewal Due Date'] = returnValue('Renewal due', dict)
+    rowdict['Status'] = returnValue('Status', dict)
+    rowdict['Owner'] = returnValue('Owner', dict)
+    rowdict['Address for service'] = returnValue('Address for service', dict)
+    rowdict['IR Contact'] = returnValue('IR Contact', dict)
+    rowdict['History'] = returnValue('History', dict)
+    rowdict['Goods and services'] = returnValue('Goods and services', dict)
+    rowdict['Indexing constituents image'] = returnValue('Indexing constituents image', dict)
+    rowdict['Indexing constituents word'] = returnValue('Indexing constituents word', dict)
+    if 'Convention details' in dict:
+        rowdict['Convention date'] = returnValue('Date', dict['Convention details'])
+        rowdict['Convention number'] = returnValue('Number', dict['Convention details'])
+        rowdict['Convention country'] = returnValue('Country', dict['Convention details'])
+    else:
+        rowdict['Convention date'] = ''
+        rowdict['Convention number'] = ''
+        rowdict['Convention country'] = ''
+
+    return rowdict
+
+def ouputCSV(dict, filename, mode):
+    global isHeader
+    with open(filename, mode, newline='\n') as file:
+        csvWriter = csv.DictWriter(file, fieldnames=fieldnames, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         if isHeader:
             csvWriter.writeheader()
             isHeader = False
-        csvWriter.writerow(rowdict)
+        csvWriter.writerow(dict)
 
 def argsParse():
     parser = argparse.ArgumentParser()
@@ -84,19 +98,17 @@ def argsParse():
     parser.add_argument('--json', help="output json format", default=True)
     return vars(parser.parse_args())
 
-
 def scrap_Unit(trademark, filename):
+    print(trademark)
     url = BASE_URL + trademark + "/details??a=1&h=1"
-    print(url)
-    session = requests.session()
     res = session.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
     divs = soup.find_all('div', {'class': 'box-with-shadow'})
     extracted_data = {'ID' : trademark}
 
-    """ Retrieve First table in first Div:  Words, Image, Image description, Status, Priority date, Class(Classes), Kind"""
-    table_body = divs[0].find_all('table')
-    trs = table_body[1].find_all('tr')
+    """ Step 1. Retrieve First table in first Div:  Words, Image, Image description, Status, Priority date, Class(Classes), Kind"""
+    table_bodys = divs[0].find_all('table')
+    trs = table_bodys[1].find_all('tr')
 
     for tr in trs:
         key = tr.find('th').text.replace('\n', '')
@@ -104,24 +116,22 @@ def scrap_Unit(trademark, filename):
         if key == '': continue
         extracted_data[key] = value
     ## especially get image url
-    images = table_body[1].find_all('img')
+    imagetags = table_bodys[1].find_all('img')
     iamge_urls = []
+    for image in imagetags:
+        iamge_urls.append(image.get('src').replace('\n', ' '))
+    ## especially get video url
+    images = table_bodys[1].find_all('video')
     for image in images:
-        iamge_urls.append(image.get('src'))
-    extracted_data['Image'] = iamge_urls
+        iamge_urls.append(image.get('src').replace('\n', ' '))
+    extracted_data['Image'] = "||" .join(iamge_urls)
 
-    """ Retrieve Second table in first Div: Dates
+    """ Step 2.  Retrieve Second table in first Div: Dates
     {
-        Renewal due,
-        Registration advertised,
-        Entered on Register,
-        Acceptance advertised,
-        Acceptance,
-        Filing,
-        Registered from
+        Renewal due, Registration advertised, Entered on Register, Acceptance advertised, Acceptance, Filing, Registered from
     }
     """
-    trs = table_body[2].find_all('tr')
+    trs = table_bodys[2].find_all('tr')
     for tr in trs:
         tds = tr.find_all('td')
         if len(tds) == 1: continue
@@ -130,22 +140,29 @@ def scrap_Unit(trademark, filename):
         if key == '': continue
         extracted_data[key] = value
 
-    """ Retrieve in Second Div : Owner, Address for service """
+    """ Step 3. Retrieve in Second Div : Owner, Address for service """
     tds = divs[1].find_all('td')
     Owner = tds[0].text.replace('\n', ' ')
     Address = tds[1].text.replace('\n', ' ')
     extracted_data['Owner'] = Owner
     extracted_data['Address for service'] = Address
 
-    """ Retreive Third Div : Goods & Services"""
+    """ Step 4. Retreive Third Div : Goods & Services"""
     goods_services = soup.find_all("div", {'class': 'box-with-shadow long-content-container'})[0]\
                          .find_all('div', {'class': 'goods-service'})
     values = []
     for services in goods_services:
         values.append(services.text.replace('\n', ' '))
-    extracted_data['Goods and services'] = values
+    extracted_data['Goods and services'] = "||".join(values)
+
+    """ Step 5. Extract IR Contact """
+    if len(divs[2].get('class'))==1:
+        trs = divs[2].find_all('tr')
+        if len(trs) > 1:
+            extracted_data['IR Contact'] = trs[1].text.replace('\n',' ')
 
     """
+        Step 6.
         get all data of div with class name  'box-with-shadow'
     """
     for i in range(3, len(divs)):
@@ -158,11 +175,11 @@ def scrap_Unit(trademark, filename):
                     tds = tr.find_all('td')
                     if len(tds) == 1: continue
                     key = tds[0].text.replace('\n', '')
-                    value = tds[1].text.replace('\n', ' ')
+                    value = tds[1].text.replace('\n', ' ').replace('"',"'")
                     value_dicts[key] = value
                 extracted_data[key_dict] = value_dicts
 
-    """ History and publication details """
+    """ Step 7. History and publication details """
     tbodys = soup.find('div', id='toggleHistoryTable').find_all('tbody')
     history = []
     for tbody in tbodys:
@@ -171,24 +188,18 @@ def scrap_Unit(trademark, filename):
         if len(tds) == 0: continue
         key = tds[0].text.replace('\n', '')
         value = tds[1].text.replace('\n', ' ')
-        vals = {}
-        vals['date'] = key
-        vals['description'] = value
+        history.append('-'.join([key, value]))
 
         if len(trs) > 1:
-            eventval = {}
             ps = trs[1].find_all('p')
+            pubvalue = ""
             for p in ps:
-                pubvalues = p.text.split('\n\n')
-                key = pubvalues[0].replace('\n', '')
-                pubvalue = '' if len(pubvalues) == 1 else pubvalues[1]
-                pubvalue = pubvalue.replace('\n', ' ')
-                eventval[key] = pubvalue
-            vals['detail'] = eventval
-        history.append(vals)
-    extracted_data['History'] = history
+                pubval = p.text.replace('\n',' ')
+                pubvalue = pubvalue + pubval
+            history.append(pubvalue)
+    extracted_data['History'] = '||'.join(history)
 
-    """ Get Indexing constituents """
+    """ Step 8. Get Indexing constituents """
     tables = soup.find('div',{'class':'box-with-shadow row cf'}).find_all('table')
     if len(tables) > 1:
         ## get Words
@@ -197,9 +208,9 @@ def scrap_Unit(trademark, filename):
         for tr in trs:
             tds = tr.find_all('td')
             if len(tds) > 1:
-                snippet = ":".join([tds[0].text,tds[1].text])
+                snippet = ":".join([tds[0].text.replace('\n', ' '),tds[1].text.replace('\n', ' ')])
             else:
-                snippet = ":".join([tds[0].text])
+                snippet = ":".join([tds[0].text.replace('\n', ' ')])
             words.append(snippet)
         ## get Image
         images = []
@@ -207,25 +218,36 @@ def scrap_Unit(trademark, filename):
         for tr in trs:
             tds = tr.find_all('td')
             if len(tds) > 1:
-                snippet = ":".join([tds[0].text, tds[1].text])
+                snippet = "-".join([tds[0].text.replace('\n', ' '), tds[1].text.replace('\n', ' ')])
             else:
-                snippet = ":".join([tds[0].text])
+                snippet = "-".join([tds[0].text.replace('\n', ' ')])
             images.append(snippet)
-        extracted_data['Indexing constituents image'] = images
-        extracted_data['Indexing constituents word'] = words
+        extracted_data['Indexing constituents image'] = "||".join(images)
+        extracted_data['Indexing constituents word'] = "||".join(words)
 
+    """ create dict from extracted_data """
+    rowdict = AdjustData(extracted_data)
+
+    """ Output as csv or json """
     if filename:
-        ouputCSV(extracted_data,filename)
-    # print(json.dumps(extracted_data))
-
+        ouputCSV(rowdict,filename, 'a')
+    else:
+        print(json.dumps(rowdict))
 
 if __name__ == "__main__":
     args = argsParse()
 
+    """
+    # args for test
+    args = {
+        'csv' : 'outut.csv',
+        'trademark' : '1594492',
+        'verbose' : 'id'
+    }
+    """
     if args['csv'] == None and args['json']==None:
         print("--csv or --json required")
         sys.exit()
-
     print("start scraping")
     if args['verbose'] == 'file':
         file = open(args['trademark'], 'r')
